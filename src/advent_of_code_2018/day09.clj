49f83fcf9ec4064ec952e02bf21636efa8c4c239
OHTA Shogo <athos0220@gmail.com>
2018-12-09 21:55:48 +0900

    Add solution for Day 9

---
+++ src/advent_of_code_2018/day09.clj
@@ -0,0 +1,78 @@
+(ns advent-of-code-2018.day09)
+
+(defprotocol INode
+  (value [this])
+  (left [this])
+  (right [this])
+  (set-left! [this node])
+  (set-right! [this node]))
+
+(deftype Node [value ^:volatile-mutable left ^:volatile-mutable right]
+  INode
+  (value [this] (.-value this))
+  (left [this] (.-left this))
+  (right [this] (.-right this))
+  (set-left! [this node]
+    (set! (.-left this) node))
+  (set-right! [this node]
+    (set! (.-right this) node))
+  clojure.lang.ISeq
+  (seq [this]
+    (letfn [(rec [^Node node]
+              (lazy-seq
+               (when-not (identical? node this)
+                 (cons (.-value node) (rec (.-right node))))))]
+      (cons value (rec (.-right this))))))
+
+(defn init-nodes []
+  (let [zero (->Node 0 nil nil)
+        one (->Node 1 nil nil)]
+    (set-left! zero one)
+    (set-right! zero one)
+    (set-left! one zero)
+    (set-right! one zero)
+    one))
+
+(defn add-value! [current-node val]
+  (let [node (->Node val nil nil)
+        r (right current-node)
+        rr (right r)]
+    (set-right! r node)
+    (set-left! node r)
+    (set-right! node rr)
+    (set-left! rr node)
+    node))
+
+(defn remove-node! [current-node]
+  (let [l (left current-node)
+        r (right current-node)]
+    (set-left! r l)
+    (set-right! l r)
+    r))
+
+(defn num->player [players i]
+  (inc (mod (dec i) (count players))))
+
+(defn step [{:keys [nodes players] :as state} i]
+  (if (= (rem i 23) 0)
+    (let [player (num->player players i)
+          node (->> (iterate left nodes)
+                    (drop 7)
+                    first)
+          v (value node)]
+      (assoc state
+             :nodes (remove-node! node)
+             :players (update players player + i v)))
+    (let [next-node (add-value! nodes i)]
+      (assoc state :nodes next-node))))
+
+(defn solve1 [n-players last]
+  (let [nodes (init-nodes)
+        players (zipmap (range 1 (inc n-players)) (repeat 0))]
+    (loop [state {:nodes nodes :players players}, i 2]
+      (if (<= i last)
+        (recur (step state i) (inc i))
+        (val (apply max-key val (:players state)))))))
+
+(defn solve2 [n-players last]
+  (solve1 n-players last))
---
+++ test/advent_of_code_2018/day09_test.clj
@@ -0,0 +1,15 @@
+(ns advent-of-code-2018.day09-test
+  (:require [advent-of-code-2018.day09 :as day09]
+            [clojure.test :refer [deftest is]]))
+
+(deftest solve1-test
+  (is (= 32 (day09/solve1 9 25)))
+  (is (= 8317 (day09/solve1 10 1618)))
+  (is (= 146373 (day09/solve1 13 7999)))
+  (is (= 2764 (day09/solve1 17 1104)))
+  (is (= 54718 (day09/solve1 21 6111)))
+  (is (= 37305 (day09/solve1 30 5807)))
+  (is (= 410375 (day09/solve1 439 71307))))
+
+(deftest solve2-test
+  (is (= 3314195047 (day09/solve2 439 7130700))))
